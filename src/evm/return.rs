9071effa6cc80d76eb3d7d5720503e142d1ff431
Alex Z <a.zarudnyy@matterlabs.dev>
2022-05-12 20:49:35 +0200

    Return an empty array of immutables from the constructor

--- src/evm/return.rs
+++ src/evm/return.rs
@@ -2,6 +2,8 @@
 //! Translates the transaction return operations.
 //!
 
+use crate::context::address_space::AddressSpace;
+use crate::context::code_type::CodeType;
 use crate::context::function::intrinsic::Intrinsic as IntrinsicFunction;
 use crate::context::Context;
 use crate::Dependency;
@@ -16,10 +18,39 @@ pub fn r#return<'ctx, 'dep, D>(
 where
     D: Dependency,
 {
-    let offset = arguments[0].into_int_value();
-    let size = arguments[1].into_int_value();
+    match context.code_type.expect("Always exists") {
+        CodeType::Deploy => {
+            let immutables_offset_pointer = context.access_memory(
+                context.field_const(0),
+                AddressSpace::Heap,
+                "immutables_offset_pointer",
+            );
+            context.build_store(
+                immutables_offset_pointer,
+                context.field_const(compiler_common::SIZE_FIELD as u64),
+            );
 
-    context.build_exit(IntrinsicFunction::Return, offset, size);
+            let immutables_length_pointer = context.access_memory(
+                context.field_const(compiler_common::SIZE_FIELD as u64),
+                AddressSpace::Heap,
+                "immutables_length_pointer",
+            );
+            context.build_store(immutables_length_pointer, context.field_const(0));
+
+            context.build_exit(
+                IntrinsicFunction::Return,
+                context.field_const(0),
+                context.field_const((compiler_common::SIZE_FIELD * 2) as u64),
+            );
+        }
+        CodeType::Runtime => {
+            let offset = arguments[0].into_int_value();
+            let size = arguments[1].into_int_value();
+
+            context.build_exit(IntrinsicFunction::Return, offset, size);
+        }
+    }
+
     Ok(None)
 }
 
