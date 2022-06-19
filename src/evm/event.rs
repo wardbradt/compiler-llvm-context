0e2325d19d4852afa21f3b7d1b076075e0fd0def
Alex Z <a.zarudnyy@matterlabs.dev>
2022-06-19 22:36:40 +0200

    Refactor the event generator

--- src/evm/event.rs
+++ src/evm/event.rs
@@ -21,200 +21,253 @@ pub fn log<'ctx, D>(
 where
     D: Dependency,
 {
-    let intrinsic = context.get_intrinsic_function(IntrinsicFunction::Event);
+    if topics.len() % 2 != 0 {
+        topic_odd_number(context, range_start, length, topics)?;
+        return Ok(None);
+    }
 
-    let exit_block = context.append_basic_block("event_exit");
+    let data_empty_block = context.append_basic_block("event_even_data_empty");
+    let data_not_empty_block = context.append_basic_block("event_even_data_not_empty");
+    let join_block = context.append_basic_block("event_even_data_join");
 
+    let data_empty_condition = context.builder().build_int_compare(
+        inkwell::IntPredicate::EQ,
+        length,
+        context.field_const(0),
+        "event_even_data_empty_condition",
+    );
+    context.build_conditional_branch(data_empty_condition, data_empty_block, data_not_empty_block);
+
+    context.set_basic_block(data_empty_block);
+    topic_even_number_data_empty(context, topics.clone())?;
+    context.build_unconditional_branch(join_block);
+
+    context.set_basic_block(data_not_empty_block);
+    topic_even_number_data_not_empty(context, range_start, length, topics)?;
+    context.build_unconditional_branch(join_block);
+
+    context.set_basic_block(join_block);
+    Ok(None)
+}
+
+///
+/// Handles the even number of topics and empty data.
+///
+pub fn topic_even_number_data_empty<'ctx, D>(
+    context: &mut Context<'ctx, D>,
+    topics: Vec<inkwell::values::IntValue<'ctx>>,
+) -> anyhow::Result<()>
+where
+    D: Dependency,
+{
+    let event_initializer = context.field_const(topics.len() as u64);
+
+    if topics.is_empty() {
+        context.build_call(
+            context.get_intrinsic_function(IntrinsicFunction::Event),
+            &[
+                event_initializer.as_basic_value_enum(),
+                context.field_const(0).as_basic_value_enum(),
+                context.field_const(1).as_basic_value_enum(),
+            ],
+            "event_even_data_empty_init_with_no_topics",
+        );
+        return Ok(());
+    }
+
+    let mut topic_index = 0;
+    context.build_call(
+        context.get_intrinsic_function(IntrinsicFunction::Event),
+        &[
+            event_initializer.as_basic_value_enum(),
+            topics[topic_index].as_basic_value_enum(),
+            context.field_const(1).as_basic_value_enum(),
+        ],
+        "event_even_data_empty_init_with_first_topic",
+    );
+    topic_index += 1;
+    while topics.len() - topic_index >= 2 {
+        context.build_call(
+            context.get_intrinsic_function(IntrinsicFunction::Event),
+            &[
+                topics[topic_index].as_basic_value_enum(),
+                topics[topic_index + 1].as_basic_value_enum(),
+                context.field_const(0).as_basic_value_enum(),
+            ],
+            "event_even_data_empty_next_two_topics",
+        );
+        topic_index += 2;
+    }
+    context.build_call(
+        context.get_intrinsic_function(IntrinsicFunction::Event),
+        &[
+            topics[topic_index].as_basic_value_enum(),
+            context.field_const(0).as_basic_value_enum(),
+            context.field_const(0).as_basic_value_enum(),
+        ],
+        "event_even_data_empty_last_topic",
+    );
+
+    Ok(())
+}
+
+///
+/// Handles the even number of topics and non-empty data.
+///
+pub fn topic_even_number_data_not_empty<'ctx, D>(
+    context: &mut Context<'ctx, D>,
+    range_start: inkwell::values::IntValue<'ctx>,
+    length: inkwell::values::IntValue<'ctx>,
+    topics: Vec<inkwell::values::IntValue<'ctx>>,
+) -> anyhow::Result<()>
+where
+    D: Dependency,
+{
     let topics_length = context.field_const(topics.len() as u64);
     let data_length_shifted = context.builder().build_left_shift(
         length,
         context.field_const((compiler_common::BITLENGTH_X32) as u64),
-        "event_data_length_shifted",
+        "event_even_data_length_shifted",
     );
-    let event_initializer =
-        context
-            .builder()
-            .build_int_add(topics_length, data_length_shifted, "event_initializer");
+    let event_initializer = context.builder().build_int_add(
+        topics_length,
+        data_length_shifted,
+        "event_even_initializer",
+    );
 
-    let (range_start, length) = if topics.len() % 2 == 0 {
-        let range_start_pointer =
-            context.build_alloca(context.field_type(), "event_odd_range_start_pointer");
-        let length_pointer = context.build_alloca(context.field_type(), "event_odd_length_pointer");
-
-        let data_empty_block = context.append_basic_block("event_odd_data_empty");
-        let data_not_empty_block = context.append_basic_block("event_odd_data_not_empty");
-        let loop_block = context.append_basic_block("event_odd_data_loop");
-
-        let data_empty_condition = context.builder().build_int_compare(
-            inkwell::IntPredicate::EQ,
-            length,
-            context.field_const(0),
-            "event_odd_data_empty_condition",
+    let pointer = context.access_memory(
+        range_start,
+        AddressSpace::Heap,
+        "event_even_first_value_pointer",
+    );
+    let value = context.build_load(pointer, "event_even_first_value");
+    if topics.is_empty() {
+        context.build_call(
+            context.get_intrinsic_function(IntrinsicFunction::Event),
+            &[
+                event_initializer.as_basic_value_enum(),
+                value,
+                context.field_const(1).as_basic_value_enum(),
+            ],
+            "event_even_data_not_empty_init_with_value",
         );
-        context.build_conditional_branch(
-            data_empty_condition,
-            data_empty_block,
-            data_not_empty_block,
-        );
-
-        context.set_basic_block(data_empty_block);
-        if topics.is_empty() {
-            context.build_call(
-                intrinsic,
-                &[
-                    event_initializer.as_basic_value_enum(),
-                    context.field_const(0).as_basic_value_enum(),
-                    context.field_const(1).as_basic_value_enum(),
-                ],
-                "event_odd_data_empty_init_with_no_topics",
-            );
-            context.build_unconditional_branch(exit_block);
-        } else {
-            let mut topic_index = 0;
-            context.build_call(
-                intrinsic,
-                &[
-                    event_initializer.as_basic_value_enum(),
-                    topics[topic_index].as_basic_value_enum(),
-                    context.field_const(1).as_basic_value_enum(),
-                ],
-                "event_odd_data_empty_init_with_first_topic",
-            );
-            topic_index += 1;
-            while topics.len() - topic_index >= 2 {
-                context.build_call(
-                    intrinsic,
-                    &[
-                        topics[topic_index].as_basic_value_enum(),
-                        topics[topic_index + 1].as_basic_value_enum(),
-                        context.field_const(0).as_basic_value_enum(),
-                    ],
-                    "event_odd_data_empty_next_two_topics",
-                );
-                topic_index += 2;
-            }
-            context.build_call(
-                intrinsic,
-                &[
-                    topics[topic_index].as_basic_value_enum(),
-                    context.field_const(0).as_basic_value_enum(),
-                    context.field_const(0).as_basic_value_enum(),
-                ],
-                "event_odd_data_empty_last_topic",
-            );
-            context.build_store(range_start_pointer, range_start);
-            context.build_store(length_pointer, length);
-            context.build_unconditional_branch(loop_block);
-        }
-
-        context.set_basic_block(data_not_empty_block);
-        let pointer = context.access_memory(
-            range_start,
-            AddressSpace::Heap,
-            "event_odd_first_value_pointer",
-        );
-        let value = context.build_load(pointer, "event_odd_first_value");
-        if topics.is_empty() {
-            context.build_call(
-                intrinsic,
-                &[
-                    event_initializer.as_basic_value_enum(),
-                    value,
-                    context.field_const(1).as_basic_value_enum(),
-                ],
-                "event_odd_data_not_empty_init_with_value",
-            );
-        } else {
-            let mut topic_index = 0;
-            context.build_call(
-                intrinsic,
-                &[
-                    event_initializer.as_basic_value_enum(),
-                    topics[topic_index].as_basic_value_enum(),
-                    context.field_const(1).as_basic_value_enum(),
-                ],
-                "event_odd_data_not_empty_init_with_topic",
-            );
-            topic_index += 1;
-            while topics.len() - topic_index >= 2 {
-                context.build_call(
-                    intrinsic,
-                    &[
-                        topics[topic_index].as_basic_value_enum(),
-                        topics[topic_index + 1].as_basic_value_enum(),
-                        context.field_const(0).as_basic_value_enum(),
-                    ],
-                    "event_odd_data_not_empty_next_two_topics",
-                );
-                topic_index += 2;
-            }
-            context.build_call(
-                intrinsic,
-                &[
-                    topics[topic_index].as_basic_value_enum(),
-                    value,
-                    context.field_const(0).as_basic_value_enum(),
-                ],
-                "event_odd_data_not_empty_last_topic",
-            );
-        }
-
-        context.build_store(
-            range_start_pointer,
-            context.builder().build_int_add(
-                range_start,
-                context.field_const(compiler_common::SIZE_FIELD as u64),
-                "event_odd_range_start_after_first",
-            ),
-        );
-        context.build_store(
-            length_pointer,
-            context.builder().build_int_sub(
-                length,
-                context.field_const(compiler_common::SIZE_FIELD as u64),
-                "event_odd_length_without_first",
-            ),
-        );
-        context.build_unconditional_branch(loop_block);
-
-        context.set_basic_block(loop_block);
-        let range_start = context
-            .build_load(range_start_pointer, "event_odd_range_start_joined")
-            .into_int_value();
-        let length = context
-            .build_load(length_pointer, "event_odd_length_joined")
-            .into_int_value();
-        (range_start, length)
     } else {
         let mut topic_index = 0;
         context.build_call(
-            intrinsic,
+            context.get_intrinsic_function(IntrinsicFunction::Event),
             &[
                 event_initializer.as_basic_value_enum(),
                 topics[topic_index].as_basic_value_enum(),
                 context.field_const(1).as_basic_value_enum(),
             ],
-            "event_even_init_with_topic",
+            "event_even_data_not_empty_init_with_topic",
         );
         topic_index += 1;
         while topics.len() - topic_index >= 2 {
             context.build_call(
-                intrinsic,
+                context.get_intrinsic_function(IntrinsicFunction::Event),
                 &[
                     topics[topic_index].as_basic_value_enum(),
                     topics[topic_index + 1].as_basic_value_enum(),
                     context.field_const(0).as_basic_value_enum(),
                 ],
-                "event_even_next_two_topics",
+                "event_even_data_not_empty_next_two_topics",
             );
             topic_index += 2;
         }
-        (range_start, length)
-    };
+        context.build_call(
+            context.get_intrinsic_function(IntrinsicFunction::Event),
+            &[
+                topics[topic_index].as_basic_value_enum(),
+                value,
+                context.field_const(0).as_basic_value_enum(),
+            ],
+            "event_even_data_not_empty_last_topic",
+        );
+    }
 
+    let range_start = context.builder().build_int_add(
+        range_start,
+        context.field_const(compiler_common::SIZE_FIELD as u64),
+        "event_even_range_start_after_first",
+    );
+    let length = context.builder().build_int_sub(
+        length,
+        context.field_const(compiler_common::SIZE_FIELD as u64),
+        "event_even_length_without_first",
+    );
+    data_loop(context, range_start, length)?;
+
+    Ok(())
+}
+
+///
+/// Handles the odd number of topics.
+///
+pub fn topic_odd_number<'ctx, D>(
+    context: &mut Context<'ctx, D>,
+    range_start: inkwell::values::IntValue<'ctx>,
+    length: inkwell::values::IntValue<'ctx>,
+    topics: Vec<inkwell::values::IntValue<'ctx>>,
+) -> anyhow::Result<()>
+where
+    D: Dependency,
+{
+    let topics_length = context.field_const(topics.len() as u64);
+    let data_length_shifted = context.builder().build_left_shift(
+        length,
+        context.field_const((compiler_common::BITLENGTH_X32) as u64),
+        "event_odd_data_length_shifted",
+    );
+    let event_initializer = context.builder().build_int_add(
+        topics_length,
+        data_length_shifted,
+        "event_odd_initializer",
+    );
+
+    let mut topic_index = 0;
+    context.build_call(
+        context.get_intrinsic_function(IntrinsicFunction::Event),
+        &[
+            event_initializer.as_basic_value_enum(),
+            topics[topic_index].as_basic_value_enum(),
+            context.field_const(1).as_basic_value_enum(),
+        ],
+        "event_odd_init_with_topic",
+    );
+    topic_index += 1;
+    while topics.len() - topic_index >= 2 {
+        context.build_call(
+            context.get_intrinsic_function(IntrinsicFunction::Event),
+            &[
+                topics[topic_index].as_basic_value_enum(),
+                topics[topic_index + 1].as_basic_value_enum(),
+                context.field_const(0).as_basic_value_enum(),
+            ],
+            "event_odd_next_two_topics",
+        );
+        topic_index += 2;
+    }
+    data_loop(context, range_start, length)?;
+
+    Ok(())
+}
+
+///
+/// Generates the data writing loop.
+///
+pub fn data_loop<'ctx, D>(
+    context: &mut Context<'ctx, D>,
+    range_start: inkwell::values::IntValue<'ctx>,
+    length: inkwell::values::IntValue<'ctx>,
+) -> anyhow::Result<()>
+where
+    D: Dependency,
+{
     let condition_block = context.append_basic_block("event_loop_condition");
     let body_block = context.append_basic_block("event_loop_body");
     let increment_block = context.append_basic_block("event_loop_increment");
+    let join_block = context.append_basic_block("event_loop_join");
 
     let index_pointer = context.build_alloca(context.field_type(), "event_loop_index_pointer");
     let range_end = context
@@ -233,7 +286,7 @@ where
         range_end,
         "event_loop_condition",
     );
-    context.build_conditional_branch(condition, body_block, exit_block);
+    context.build_conditional_branch(condition, body_block, join_block);
 
     context.set_basic_block(increment_block);
     let index_value = context
@@ -284,7 +337,7 @@ where
     );
     let value_2 = context.build_load(value_2_pointer, "event_loop_value_2");
     context.build_call(
-        intrinsic,
+        context.get_intrinsic_function(IntrinsicFunction::Event),
         &[
             value_1,
             value_2,
@@ -302,7 +355,7 @@ where
     );
     let value_1 = context.build_load(value_1_pointer, "event_loop_value_1");
     context.build_call(
-        intrinsic,
+        context.get_intrinsic_function(IntrinsicFunction::Event),
         &[
             value_1,
             context.field_const(0).as_basic_value_enum(),
@@ -312,7 +365,6 @@ where
     );
     context.build_unconditional_branch(increment_block);
 
-    context.set_basic_block(exit_block);
-
-    Ok(None)
+    context.set_basic_block(join_block);
+    Ok(())
 }
